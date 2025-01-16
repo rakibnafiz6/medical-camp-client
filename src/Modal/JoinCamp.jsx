e224ceaf63a640dab88ceb8888e2ee894aef7cc9
nafiz rakib <rakibnafiz6@gmail.com>
2025-01-16 15:18:28 +0600

    added the register modal and stored participant data in db

---
+++ src/Modal/JoinCamp.jsx
@@ -0,0 +1,207 @@
+import axios from 'axios';
+import useAuth from '../hooks/useAuth';
+import Swal from 'sweetalert2';
+
+const JoinCamp = ({ camps }) => {
+    const { user } = useAuth();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const campName = form.campName.value;
+        const campFees = form.campFees.value;
+        const location = form.location.value;
+        const professionalName = form.professionalName.value;
+        const participantName = form.participantName.value;
+        const participantEmail = form.participantEmail.value;
+        const age = form.age.value;
+        const phoneNumber = form.phoneNumber.value;
+        const gender = form.gender.value;
+        const contact = form.contact.value;
+
+
+
+        const JoinCamp = {
+            campName, campFees, location, professionalName, participantName, participantEmail, age, phoneNumber, gender, contact
+        }
+        
+        axios.post(`${import.meta.env.VITE_API_URL}/joins`, JoinCamp)
+            .then(res => {
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Register data stored in db",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    form.reset();
+                    document.getElementById('my_modal_5').close();
+                    //   navigate('/myVolunteer')
+                }
+            })
+    }
+
+
+    return (
+        <div>
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <button className="btn btn-primary" onClick={() => document.getElementById('my_modal_5').showModal()}>Join Camp</button>
+            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <form onSubmit={handleSubmit}>
+                        {/* Camp Name */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Camp Name</label>
+                            <input
+                                type="text"
+                                name='campName'
+                                // defaultValue={camps.campName}
+                                value={camps.campName}
+                                readOnly
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter camp name"
+                            />
+                        </div>
+
+                        {/* Camp Fees */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Camp Fees</label>
+                            <input
+                                type="number"
+                                name='campFees'
+                                // defaultValue={camps.fees}
+                                value={camps.fees}
+                                readOnly
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter camp fees"
+                            />
+                        </div>
+
+
+                        {/* Location */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Location</label>
+                            <input
+                                type="text"
+                                name='location'
+                                // defaultValue={camps.location}
+                                value={camps.location}
+                                readOnly
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter location"
+                            />
+                        </div>
+
+
+
+                        {/* Healthcare Professional Name */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Healthcare Professional</label>
+                            <input
+                                type="text"
+                                name='professionalName'
+                                // defaultValue={camps.professionalName}
+                                value={camps.professionalName}
+                                readOnly
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter healthcare professional name"
+                            />
+                        </div>
+
+                        {/* Participant Name */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Participant Name</label>
+                            <input
+                                type="text"
+                                name='participantName'
+                                defaultValue={user?.displayName}
+                                readOnly
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter participant name"
+                            />
+                        </div>
+
+                        {/* Participant Email */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Participant Email</label>
+                            <input
+                                type="email"
+                                defaultValue={user?.email}
+                                readOnly
+                                name='participantEmail'
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter participant email"
+                            />
+                        </div>
+                        {/* Age */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Age</label>
+                            <input
+                                type="text"
+                                name='age'
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter your age"
+                                required
+                            />
+                        </div>
+                        {/* Phone Number */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Phone Number</label>
+                            <input
+                                type="number"
+                                name='phoneNumber'
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter phone number"
+                                required
+                            />
+                        </div>
+
+                        {/* Gender */}
+                        <div className="mb-4">
+                            <select
+                                name='gender'
+                                required
+                                className="w-full px-4 py-2 border rounded"
+                            >
+                                <option value="">Select Gender</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div>
+
+                        {/* Emergency Contact */}
+                        <div className="mb-4">
+                            <label className="block text-gray-700">Emergency Contact</label>
+                            <input
+                                type="text"
+                                name='contact'
+                                className="w-full px-4 py-2 border rounded"
+                                placeholder="Enter emergency contact"
+                                required
+                            />
+                        </div>
+
+
+                        {/* Submit Button */}
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+                        >
+                            Register
+                        </button>
+                    </form>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </div>
+    );
+};
+
+export default JoinCamp;
--- src/Pages/CampDetails/CampDetails.jsx
+++ src/Pages/CampDetails/CampDetails.jsx
@@ -7,6 +7,7 @@ import { GiMoneyStack } from 'react-icons/gi';
 import { MdSupervisorAccount } from 'react-icons/md';
 import { SiNamebase } from 'react-icons/si';
 import { useParams } from 'react-router-dom';
+import JoinCamp from '../../Modal/JoinCamp';
 
 const CampDetails = () => {
     const { id } = useParams();
@@ -19,9 +20,7 @@ const CampDetails = () => {
         }
     })
 
-    const { campName, dateTime, description, fees, image, location, participantCount, professionalName } = camps;
-    // console.log(camps);
-
+   
     return (
         <div>
             <h2>Camp Details</h2>
@@ -29,21 +28,21 @@ const CampDetails = () => {
                 <div className="hero-content flex-col lg:flex-row w-full">
                     <div className='w-2/3'>
                         <img
-                            src={image}
+                            src={camps.image}
                             className="w-full rounded-lg shadow-2xl" />
                     </div>
                     <div className='w-1/3'>
-                        <h1 className="text-5xl font-bold">{campName}</h1>
-                        <p className='flex items-center gap-1 mb-2 mt-2'><FaLocationArrow />{location}</p>
-                        <p className='flex items-center gap-1 mb-2'><BsCalendarDateFill />{dateTime}</p>
-                        <p className='flex items-center gap-1 mb-2'><MdSupervisorAccount />{participantCount}</p>
-                        <p className='flex items-center gap-1 mb-2'><GiMoneyStack />{fees}</p>
-                        <p className='flex items-center gap-1'><SiNamebase />{professionalName}</p>
+                        <h1 className="text-5xl font-bold">{camps.campName}</h1>
+                        <p className='flex items-center gap-1 mb-2 mt-2'><FaLocationArrow />{camps.location}</p>
+                        <p className='flex items-center gap-1 mb-2'><BsCalendarDateFill />{camps.dateTime}</p>
+                        <p className='flex items-center gap-1 mb-2'><MdSupervisorAccount />{camps.participantCount}</p>
+                        <p className='flex items-center gap-1 mb-2'><GiMoneyStack />{camps.fees}</p>
+                        <p className='flex items-center gap-1'><SiNamebase />{camps.professionalName}</p>
                         <p className="py-6">
-                            {/* {description} */}
+                            {/* {camps.description} */}
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas alias modi quae cumque. Amet eligendi error, perferendis, explicabo cum aliquid veniam, illo et expedita deserunt nobis fuga incidunt tempore delectus doloremque voluptate numquam nulla laborum! Molestiae enim voluptatem placeat eos repudiandae! Architecto consectetur quis odit? Eos non voluptate rerum earum?
                         </p>
-                        <button className="btn btn-primary">Join Camp</button>
+                        <JoinCamp camps={camps}></JoinCamp>
                     </div>
                 </div>
             </div>
