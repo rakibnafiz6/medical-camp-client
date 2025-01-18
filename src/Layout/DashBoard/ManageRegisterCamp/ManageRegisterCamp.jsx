002aa5cfdb157558a503f54ac3b93d589e9efae4
nafiz rakib <rakibnafiz6@gmail.com>
2025-01-18 21:29:47 +0600

    added patch request with update confirmation status

--- src/Layout/DashBoard/ManageRegisterCamp/ManageRegisterCamp.jsx
+++ src/Layout/DashBoard/ManageRegisterCamp/ManageRegisterCamp.jsx
@@ -1,16 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const ManageRegisterCamp = () => {
 
-    const { data: registerCamps = [] } = useQuery({
+    const { data: registerCamps = [], refetch } = useQuery({
         queryKey: ['manage-register'],
         queryFn: async () => {
             const res = await axios.get(`${import.meta.env.VITE_API_URL}/manage-register`)
             return res.data;
         }
     })
-    console.log(registerCamps);
+    
+    const handleConfirmation = (id)=>{
+        console.log(id);
+        axios.patch(`${import.meta.env.VITE_API_URL}/confirmation-status/${id}`)
+        .then(res=>{
+            // console.log(res.data);
+            if (res.data.modifiedCount) {
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `Confirmation status confirm successfully`,
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+     } })
+    }
+
+
     return (
         <div>
             <h2>Manage Register Camp</h2>
@@ -35,9 +54,9 @@ const ManageRegisterCamp = () => {
                             <td>{camp.campName}</td>
                             <td>{camp.campFees}</td>
                             <td>{camp.paymentStatus}</td>
-                            <td><button className="btn btn-xs">
+                            <td>{camp.paymentStatus === 'paid'? <button onClick={()=>handleConfirmation(camp._id)} className="btn btn-xs" disabled={camp.confirmationStatus==='Confirmed'}>
                             {camp.confirmationStatus}
-                            </button></td>
+                            </button>: camp.confirmationStatus}</td>
                             <td>
                                 <button className="btn">Cancel</button>
                             </td>
